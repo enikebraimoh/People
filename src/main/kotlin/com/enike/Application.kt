1ba648e5f12a44ce7e56b443c22754c5000573f2
enikebraimoh <richardbraimoh@gmail.com>
2022-11-02 12:04:00 +0100

    started something 🚀

---
+++ src/main/kotlin/com/enike/Application.kt
@@ -0,0 +1,13 @@
+package com.enike
+
+import io.ktor.server.application.*
+import com.enike.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/enike/models/Person.kt
@@ -0,0 +1,8 @@
+package com.enike.models
+
+import kotlinx.serialization.Serializable
+
+val peopleStorage = mutableListOf<Person>()
+
+@Serializable
+data class Person ( val name : String, val age : Int, val occupation : String, val maritalStatus : String)
