030124e523e21a82e056c56eb10f9212b5cf0845
enikebraimoh <richardbraimoh@gmail.com>
2022-12-02 05:33:13 +0100

    new Approach 🥂

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,4 +30,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("io.ktor:ktor-server-auth:$ktor_version")
 }
+
